ec3aa1aab2505ebf5e0b8105d39614cdd17b1fa7
chenyanming <ymchen@che300.com>
2018-01-08 16:46:57 +0800

    init commit

---
+++ READM.md
@@ -0,0 +1,46 @@
+# sftp-after-webpack
+a webpack plugin for sftp after webpack;
+
+>example 2
+webpack.config.js  
+```javascript
+const SftpAfterWebpack = require('sftp-after-webpack');
+
+module.exports = {
+    // ...
+  plugins: [
+    new SftpAfterWebpack()
+  ]
+};
+```
+package.json  
+```json
+"sftp-config": {
+    "host": "*.*.*.*",
+    "port": "22",
+    "username": "username",
+    "password": "password",
+    "remotePath": "/path/to/remoPath"
+  },
+```
+
+> example 2
+webpack.config.js  
+```javascript
+const SftpAfterWebpack = require('sftp-after-webpack');
+
+module.exports = {
+    // ...
+  plugins: [
+    new SftpAfterWebpack({
+        "host": "*.*.*.*",
+        "port": "22",
+        "username": "username",
+        "password": "password",
+        "remotePath": "/path/to/remoPath"
+    })
+  ]
+};
+```
+
+> TODO : test mac system
---
+++ index.js
@@ -0,0 +1,79 @@
+let Client = require('ssh2-sftp-client');
+let sftp = new Client();
+let fs = require('fs');
+let path = require('path');
+
+/**
+ * 
+ * @param {本地文件夹路径} localDir 
+ * @param {文件夹} dirs 
+ * @param {文件} files 
+ */
+function GetFileAndDirList(localDir, dirs, files) {
+    var dir = fs.readdirSync(localDir);
+    for (var i = 0; i < dir.length; i++) {
+        var p = path.join(localDir, dir[i]);
+        var stat = fs.statSync(p);
+        if (stat.isDirectory()) {
+            dirs.push(p);
+            GetFileAndDirList(p, dirs, files);
+        }
+        else {
+            files.push(p);
+        }
+    }
+}
+
+function upload(options, localDirs) {
+    const remotePath = options.remotePath;
+    sftp.connect(options).then(() => {
+        // return sftp.list('/home');
+        sftp.mkdir(remotePath, true)
+        let dirs = [], files = [];
+        GetFileAndDirList(localDirs, dirs, files)
+        for (let i = 0; i < dirs.length; i++) {
+            let dir = dirs[i];
+            dir = dir.substr(localDirs.length).replace(/\\/g, '/')
+            sftp.mkdir(`${remotePath}${dir}`, true)
+
+        }
+        for (let index = 0; index < files.length; index++) {
+            let file = files[index];
+            file = file.substr(localDirs.length).replace(/\\/g, '/')
+            sftp.put(files[index], `${remotePath}${file}`, true).then(() => {
+                console.log(`upload file ${files[index]}  ====> ${remotePath}${file}`)
+                if (index === files.length - 1) {
+                    sftp.end();
+                }
+            })
+        }
+
+    }).catch((err) => {
+        console.log(err, 'connect err');
+    });
+
+}
+
+function getOptions() {
+    const pageJson = JSON.parse(fs.readFileSync('./package.json'));
+    return pageJson['sftp-config'];
+}
+class SftpAfterWebpack {
+    constructor(options) {
+        this.options = options
+
+    }
+    apply(compiler) {
+        let options = this.options;
+        compiler.plugin("done", function (stats) {
+            if (!stats.hasErrors()) {
+                const localPath = compiler.options.output.path;
+                if (!options) {
+                    options = getOptions()
+                }
+                upload(options, localPath);
+            }
+        });
+    }
+}
+module.exports = SftpAfterWebpack
